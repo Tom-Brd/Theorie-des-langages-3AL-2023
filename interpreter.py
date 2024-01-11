5442d0e8a0338039c2fdece46911a8642a235fcd
Tom-Brd <bourlard.tom.94@gmail.com>
2024-01-12 00:24:38 +0100

    feat(functions): call function is working, cannot call a function if not inside of a statement, recursivity is not working

--- interpreter.py
+++ interpreter.py
@@ -19,15 +19,18 @@ reserved = {
     'char': 'CHAR',
     'string': 'STRING',
     'bool': 'BOOL',
+    'void': 'VOID',
     'true': 'TRUE',
-    'false': 'FALSE'
+    'false': 'FALSE',
+    'function': 'FUNCTION',
+    'return': 'RETURN',
 }
 
 tokens = [
              'NUMBER', 'MINUS',
              'PLUS', 'TIMES', 'DIVIDE',
              'LPAREN', 'RPAREN', 'LBRACKET', 'RBRACKET',
-             'AND', 'OR', 'SEMICOLON', 'NAME', 'ASSIGN', 'GREATER', 'LESS', 'EQUALS', 'NOTEQUALS', 'GREATEREQUALS', 'LESSEQUALS',
+             'AND', 'OR', 'COMMA', 'SEMICOLON', 'NAME', 'ASSIGN', 'GREATER', 'LESS', 'EQUALS','NOTEQUALS', 'GREATEREQUALS', 'LESSEQUALS',
              'INCREMENT', 'DECREMENT', 'INCREASE', 'DECREASE',
              'TRUE', 'FALSE',
              "CHARCHAIN", "SIMPLE_COMMENT", "MULTI_COMMENT",
@@ -44,6 +47,7 @@ t_LBRACKET = r'\{'
 t_RBRACKET = r'\}'
 t_AND = r'&'
 t_OR = r'\|'
+t_COMMA = r','
 t_SEMICOLON = r';'
 t_ASSIGN = r'='
 t_GREATER = r'>'
@@ -99,19 +103,23 @@ lex.lex()
 
 def toamPrint(string):
     print("TOAM PRINT : ", string)
+
+
 def debug(string):
     if DEBUG:
         print("TOAM DEBUG : ", string)
 
+
 def error(string):
     print("TOAM ERROR : ", string)
+
 
 def p_start(p):
     'start : bloc'
     p[0] = ('START', p[1])
     print('Arbre de dérivation = ', p[0])
     printTreeGraph(p[1])
-    evalSyntax(p[1])
+    # evalSyntax(p[1])
     evalInst(p[1])
 
 
@@ -129,6 +137,20 @@ def p_bloc(p):
             p[0] = p[0]
         else:
             p[0] = ('bloc', p[1], 'empty')
+
+
+def p_statement_function(p):
+    '''statement : FUNCTION RETURNTYPE NAME LPAREN PARAMS RPAREN LBRACKET bloc RBRACKET'''
+    p[0] = ('function', p[2], p[3], p[5], p[8])
+
+
+def p_params(p):
+    '''PARAMS : TYPE NAME
+                | TYPE NAME COMMA PARAMS'''
+    if len(p) == 3:
+        p[0] = ('params', p[1], p[2])
+    else:
+        p[0] = ('params', p[1], p[2], p[4])
 
 
 def p_statement_expr(p):
@@ -193,14 +215,35 @@ def p_expression_number(p):
     'expression : NUMBER'
     p[0] = p[1]
 
+
 def p_expression_boolean(p):
     'expression : BOOLEAN'
     p[0] = bool(p[1])
+
 
 def p_expression_name(p):
     'expression : NAME'
     # p[0] = names[p[1]]
     p[0] = p[1]
+
+
+def p_expression_funcparams(p):
+    '''FUNCPARAMS : expression
+                | expression COMMA FUNCPARAMS'''
+
+    if len(p) == 2:
+        p[0] = ('funcparams', p[1])
+    else:
+        p[0] = ('funcparams', p[3], p[1])
+
+
+def p_expression_call_function(p):
+    '''expression : NAME LPAREN RPAREN
+        | NAME LPAREN FUNCPARAMS RPAREN'''
+    if len(p) == 4:
+        p[0] = ('call_function', p[1])
+    else:
+        p[0] = ('call_function', p[1], p[3])
 
 
 def p_type_definition(p):
@@ -211,10 +254,28 @@ def p_type_definition(p):
             | BOOL'''
     p[0] = p[1]
 
+
+def p_return_type(p):
+    '''RETURNTYPE : TYPE
+            | VOID'''
+    p[0] = p[1]
+
+
+def p_return_statement(p):
+    '''statement : RETURN expression
+        | RETURN statement
+        | RETURN'''
+    if len(p) == 3:
+        p[0] = ('return', p[2])
+    else:
+        p[0] = ('return', 'empty')
+
+
 def p_boolean_definition(p):
     '''BOOLEAN : TRUE
                 | FALSE'''
     p[0] = p[1]
+
 
 def p_expression_charchain(p):
     'expression : CHARCHAIN'
@@ -259,11 +320,18 @@ def evalExpr(t):
     if type(t) is str:
         if t[0] == '"':
             return t
-        return get_variable(t)
+        return get_variable(t) or get_function(t)
     if type(t) is int:
         return t
     if type(t) is tuple:
         match t[0]:
+            case 'funcparams':
+                if len(t) == 2:
+                    return evalExpr(t[1])
+                else:
+                    return evalExpr(t[2]), evalExpr(t[1])
+            case 'call_function':
+                return call_function(evalExpr(t[1]), evalExpr(t[2]))
             case '+':
                 return evalExpr(t[1]) + evalExpr(t[2])
             case '*':
@@ -355,6 +423,7 @@ def evalSyntaxCondition(condition):
                 return evalSyntaxCondition(sub)
     return True
 
+
 def evalSyntaxPrint(t):
     # t = 1
     # print("test");
@@ -367,6 +436,7 @@ def evalSyntaxPrint(t):
     elif type(t) is tuple:
         for sub in t:
             evalSyntaxPrint(sub)
+
 
 def evalSyntax(t):
     if type(t) is tuple:
@@ -442,6 +512,15 @@ def evalSyntax(t):
 def evalInst(t):
     if type(t) is tuple:
         match t[0]:
+            case 'return':
+                return evalExpr(t[1])
+            case 'function':
+                # t[0] = function
+                # t[1] = return type
+                # t[2] = name
+                # t[3] = tuple de paramètres
+                # t[4] = bloc d'instruction
+                functions[t[2]] = (t[1], t[3], t[4])
             case 'declare':
                 set_variable(t[2], evalExpr(t[3]))
             case 'assign':
@@ -476,16 +555,19 @@ def evalInst(t):
                     exit(f"TOAM ERROR : Impossible de convertir l'entrée '{userInput}' en {typ} : (Variable {t[1]})")
             case 'if':
                 create_scope()
+                result = None
                 if evalExpr(t[1]):
                     create_scope()
-                    evalInst(t[2])
+                    result = evalInst(t[2])
                     exit_scope()
                 else:
                     if len(t) == 4:
                         create_scope()
-                        evalInst(t[3])
+                        result = evalInst(t[3])
                         exit_scope()
                 exit_scope()
+                if result is not None:
+                    return result
             case 'while':
                 create_scope()
                 while evalExpr(t[1]):
@@ -503,8 +585,12 @@ def evalInst(t):
                     exit_scope()
                 exit_scope()
             case 'bloc':
-                evalInst(t[1])
-                evalInst(t[2])
+                # evalInst(t[1])
+                # evalInst(t[2])
+                for stmt in t[1:]:
+                    result = evalInst(stmt)
+                    if stmt and stmt[0] == 'return':
+                        return result
             case 'start':
                 evalInst(t[1])
 
@@ -513,18 +599,57 @@ import ply.yacc as yacc
 
 variables_scope_stack = [{}]
 syntax_analysis_variables_scope_stack = [{}]
+functions = {}
+
+
+def declare_variables_function(parameters, call_params, index):
+    if type(call_params) is tuple:
+        set_variable(parameters[2], call_params[index])
+    else:
+        set_variable(parameters[2], call_params)
+
+    if len(parameters) == 3:
+        return
+    return declare_variables_function(parameters[3], call_params, index + 1)
+
+
+def call_function(name, call_params):
+    if name in functions:
+
+        called_function = functions[name]
+
+        return_type = called_function[0]
+        parameters = called_function[1]
+        instructions = called_function[2]
+
+        create_scope()
+        declare_variables_function(parameters, call_params, 0)
+        result = evalInst(instructions)
+        exit_scope()
+        return result
+    exit(f"TOAM ERROR : La fonction '{name}' n'existe pas")
+
+
+def get_function(name):
+    if name in functions:
+        return name
+    # exit(f"TOAM ERROR : La fonction '{name}' n'existe pas")
+
 
 def create_scope():
     variables_scope_stack.append({})
 
+
 def exit_scope():
     variables_scope_stack.pop()
+
 
 def exist_in_scope(name):
     for scope in reversed(variables_scope_stack):
         if name in scope:
             return True
     return False
+
 
 def update_in_scope(name, value):
     for scope in reversed(variables_scope_stack):
@@ -533,27 +658,33 @@ def update_in_scope(name, value):
             return True
     return False
 
+
 def set_variable(name, value):
     if not update_in_scope(name, value):
         variables_scope_stack[-1][name] = value
+
 
 def get_variable(name):
     for scope in reversed(variables_scope_stack):
         if name in scope:
             return scope[name]
-    exit(f"Variable non définie: {name}")
+    # exit(f"Variable non définie: {name}")
+
 
 def create_syntax_scope():
     syntax_analysis_variables_scope_stack.append({})
 
+
 def exit_syntax_scope():
     syntax_analysis_variables_scope_stack.pop()
+
 
 def exist_in_syntax_scope(name):
     for scope in reversed(syntax_analysis_variables_scope_stack):
         if name in scope:
             return True
     return False
+
 
 def update_in_syntax_scope(name, value):
     for scope in reversed(syntax_analysis_variables_scope_stack):
@@ -562,9 +693,11 @@ def update_in_syntax_scope(name, value):
             return True
     return False
 
+
 def set_syntax_variable(name, value):
     if not update_in_syntax_scope(name, value):
         syntax_analysis_variables_scope_stack[-1][name] = value
+
 
 def get_syntax_variable(name):
     for scope in reversed(syntax_analysis_variables_scope_stack):
